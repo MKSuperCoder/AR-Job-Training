3bdad842bedc3165c6cc2f6aa20af05dc7e49ca2
JosephSahap <josephsahap0@gmail.com>
2025-05-05 15:35:20 -0400

    Clean up untracked build/meta files and update .gitignore

--- Assets/Coffee Scripts/CustomerRequest.cs
+++
@@ -1,7 +0,0 @@
-[System.Serializable]
-public class CustomerRequest
-{
-    public string requestText; // Example: "Make a strong, fresh coffee"
-    public int timeLimit;      // Example: 60 (seconds)
-    public string[] instructions;
-}
